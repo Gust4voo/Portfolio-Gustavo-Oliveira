9866c2a2975f872ddafa3efcf8bc0a5b919ff53e
Gustavo Oliveira <123893190+Gust4voo@users.noreply.github.com>
2023-11-09 18:17:10 +0100

    aggiornamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+
+const rotte: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rotte),
   ],
   providers: [],
   bootstrap: [AppComponent]
