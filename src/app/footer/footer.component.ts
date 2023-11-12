201b0b43b4ceeacb251df8e4fba8091d147bb696
Gustavo Oliveira <123893190+Gust4voo@users.noreply.github.com>
2023-11-12 17:53:44 +0100

    aggiornamento

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+
+  scrollToTop() {
+    window.scrollTo({ top: 0 });
+  }
 
 }
