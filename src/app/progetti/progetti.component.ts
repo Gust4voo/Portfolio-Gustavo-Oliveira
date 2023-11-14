99a1e3b5cae8c3c390705541dc06a97644a9e4a5
Gustavo Oliveira <123893190+Gust4voo@users.noreply.github.com>
2023-11-14 03:49:01 +0100

    aggiornamento

--- src/app/progetti/progetti.component.ts
+++ src/app/progetti/progetti.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-progetti',
   templateUrl: './progetti.component.html',
   styleUrls: ['./progetti.component.scss']
 })
-export class ProgettiComponent {
+export class ProgettiComponent implements OnInit{
+  constructor() { }
+
+  ngOnInit() {
+    Aos.init();
+ }
 
 }
